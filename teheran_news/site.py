0d1324d2b0d2f627d6cbd1c98c7cdf0db0785207
cesardeaptude <cperez@aptude.com>
2020-09-09 20:42:07 -0500

    TO be reviewed for PM

--- teheran_news/site.py
+++ teheran_news/site.py
@@ -40,7 +40,7 @@ class Teheran_news(Article):
                     prelink = 'https://www.tehrantimes.com/page/archive.xhtml?wide=0&ms=0&pi={}'.format(page_news)
                 else:
                     prelink = 'https://www.tehrantimes.com/page/archive.xhtml?wide=0&ms=0&pi={}&tp={}'.format(page_news,the_topic)
-                the_urls = self.pages([prelink])
+                the_urls = self.pages( [prelink] )
                 #if not the_urls or len(the_urls) < 30:
                 if not the_urls:
                     print("Is the end of the news")
