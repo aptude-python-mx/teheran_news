0a02b510d19d1f5b277daffd724d1b706562c13a
Guillermo Carranza <gcarranza@ishopss.com>
2020-09-18 19:20:28 -0500

    blank lines

--- examples/Allnews.py
+++ examples/Allnews.py
@@ -3,7 +3,6 @@ from itertools import islice
 
 
 # Get 5 news of all topics
-
 articles = Teheran_news()
 for news in islice(articles.info, 5):
     print(news.topic, news.title, news.related_news, news.created_at)
